a9fa681b7cd211d0b76c8ebc75b46dd15f4eb7a9
RKarthikSantosh <raminenikarthik0@gmail.com>
2026-06-05 09:34:13 +0530

    feat: add About Us page with developer profile components

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -6,16 +6,12 @@ import SignIn from '@/components/SignIn';
 export default function SignInPage() {
     return (
         <>
-        <div className="flex justify-center items-center min-h-screen bg-gray-800">
-            <div className="flex-col bg-gray-700 text-gray-300 p-8 rounded-lg shadow-md w-full max-w-md mb-12">
-                <h1 className="text-3xl font-bold mb-6 text-center text-gray-300">Sign In</h1>
-                <SignIn/>
-                {/* <div className='text-center pt-6'>
-                    <span className='text-black-800'>OR USE</span>
+            <div className="flex justify-center items-center min-h-screen bg-gray-800">
+                <div className="flex-col bg-gray-700 text-gray-300 p-8 rounded-lg shadow-md w-full max-w-md mb-12">
+                    <h1 className="text-3xl font-bold mb-6 text-center text-gray-300">Sign In</h1>
+                    <SignIn />
                 </div>
-                <SocialSignIn /> */}
             </div>
-        </div>
-    </>
+        </>
     );
 }
